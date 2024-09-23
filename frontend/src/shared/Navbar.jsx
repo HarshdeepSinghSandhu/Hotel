ce579ca1836a268b506b4822bc46d6f567a63f8b
Harsh choudhary <pyash4048@gmail.com>
2024-09-23 22:54:39 +0530

    added

--- frontend/src/shared/Navbar.jsx
+++ frontend/src/shared/Navbar.jsx
@@ -5,34 +5,30 @@ const Navbar = () => {
 
     const nav_routes= [
         {
-            name:"Home",
+            name:"HOME",
             path:"/home"
         },
         {
-            name:"Gallery",
+            name:"AMENITIES",
             path:""
         },
         {
-            name:"Rooms",
+            name:"SERVICES",
             path:""
         },
         {
-            name:"Bookings",
+            name:"ABOUT US",
             path:""
         },
         {
-            name:"About-Us",
-            path:""
-        },
-        {
-            name:"Contact-Us",
+            name:"RESERVATIONS",
             path:""
         }
     ]
 
   return (
-    <div className=' flex flex-row w-full py-4 justify-between px-10 items-center text-lg text-white fixed z-10'>
-      <span>Logo</span>
+    <div className=' flex flex-row w-full py-4 justify-between px-10 items-center text-[17px] text-white fixed z-10'>
+      <span><img src="../../assets/logo.png" /></span>
       <div className='flex flex-row font-semibold'>
         {nav_routes.map((e,i)=>(<NavLink key={i} className="ml-10" to={e.path}>{e.name}</NavLink>))}
       </div>
